ac67558a1225cc84b1ce6b3f2fb72d098fc12a2e
MirandaChen <yinglanc@andrew.cmu.edu>
2019-02-12 00:14:37 -0500

    implement but notice unexpected output behavior

--- extract_bridges.py
+++ extract_bridges.py
@@ -5,42 +5,40 @@
 # output a csv file that contain all the bridge nodes 
 # this output will be the vertices of the abstracted graph
 
-# Require
-# Format on input edge.csv file:
-# - the last column has field "within_boundaries" and values "TRUE" or "FALSE"
+# Dependency: header of input csv file is as follows:
+#   from,to,id,osm_way_id,osm_bridge_relation_id,osm_bridge,osm_highway,
+#   osm_label,bridge_id,distance,within_boundaries
+
+# PROBLEM TO DO
+# We expect each bridge occupies 2 nodes, but in the output file, 
+# a bridge has multiple nodes
+
 # LIBRARY
 import csv
-# GLOBALS, dependent on format of csv file
 
+# GLOBALS, dependent on format of csv file
 WITHIN_BOUND = 10
 OSM_BRIDGE = 5
-# each bridge occupies 2 nodes
+
+
 def main():
     # for simplicity, hard code filename here
     # for more general use, use command line arguments
     output = [] 
     with open('./starter_data/pgh_edges.csv', newline='') as csvfile:
         nodereader = csv.reader(csvfile)
-        next(nodereader) # skip title
-
+        header = next(nodereader) # skip title
         for row in nodereader:
-            if foo_counter > 10: break
+            # discard row if not within bound or not bridge
             if (row[WITHIN_BOUND] == "TRUE" and row[OSM_BRIDGE] == "yes"):
-                output.add(row)
-                # write the entire row to an output file 
-            # otherwise:
-                # discard the road
+                output.append(row)
+            
+    with open('./processed_data/bridges.csv', mode='w', newline='') as csvfile:
+        bridge_writer = csv.writer(csvfile, delimiter=',')
+        bridge_writer.writerow(header)
+        for row in output:
+            bridge_writer.writerow(row)
     return 0        
             
-
-    # finish writing the output file 
-    # with open('?.csv', mode='w', newline='') as csvfile:
-    #     vertex_writer = csv.writer(csvfile, delimiter=',',
-    #                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #     for vertices in v:
-    #         vertex_writer.writerow(v[])
-    #         vertex_writer.writerow(v[])
-    
-
 if __name__ == '__main__':
     main()
